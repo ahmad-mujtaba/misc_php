56cd4d2f3512b5e2ad129ffa0993b9e795f4e8ca
Mujtaba Ahmad <mujtaba.ahmad91@gmail.com>
2019-01-28 11:06:13 +0530

    Update t.php

--- t.php
+++ t.php
@@ -7,7 +7,7 @@ if(isset($_GET['id'])) {
 	$data .= 'id = '.$_GET['id'] . ', t = '.time().', data = '.$_GET['data'].', ua = '.$_SERVER['HTTP_USER_AGENT'];
 	fwrite($fp, $data.PHP_EOL);
 	fclose($fp);
-	echo $data;
+	echo 'OK'; //$data;
 }
 
 
