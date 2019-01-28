e537e4a10378eabd5956d3b3c71c8fb39352a3fe
Mujtaba Ahmad <mujtaba.ahmad91@gmail.com>
2019-01-28 10:33:15 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,19 @@
 <?php
-
-
-
 if(isset($_GET['id'])) {
-
-$fp = fopen('log.txt', 'a+');
-fwrite($fp, $_GET['id']);
-fclose($fp);
-
-echo $_GET['id'];
+	$fp = fopen('t.txt','a+');
 
 
-
-
+	$data = '';
+	$data .= 'id = '.$_GET['id'] . ', t = '.time().', data = '.$_GET['data'].', ua = '.$_SERVER['HTTP_USER_AGENT'];
+	fwrite($fp, $data.PHP_EOL);
+	fclose($fp);
+	echo $data;
 }
 
 
-
+if(isset($_GET['clear'])) {
+	$fp = fopen('t.txt','w+');
+	fwrite($fp, '');
+	fclose($fp);
+}
 ?>
